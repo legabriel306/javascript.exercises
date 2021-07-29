c621db225805f7e2933315747708ce2a78aefaf6
Leandro Gabriel <enviaprolegabriel@gmail.com>
2021-07-29 11:11:09 -0300

    ex02-update

--- exercises/ex02/ex02.js
+++ exercises/ex02/ex02.js
@@ -2,7 +2,8 @@ function finalizar() {
     let res = document.querySelector('section#res');
     let emprestimo = Number(document.querySelector('input#emprestimo').value);
     let parcela = Number(document.querySelector('input#parcela').value);
-    let aumento = emprestimo * 1.20;
+    let juros = Number(document.querySelector('input#juros').value);
+    let aumento = emprestimo * ((juros / 100) + 1);
     let total = aumento / parcela;
     res.innerHTML = `Vai pagar <strong>${parcela}</strong> parcelas de <strong>R$${total}</strong>`;
 }

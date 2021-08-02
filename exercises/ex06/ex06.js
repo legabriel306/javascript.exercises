4adec35addce396a1b3b3c23d8c5cb809c5e7596
Leandro Gabriel <enviaprolegabriel@gmail.com>
2021-08-02 17:16:26 -0300

    Update 2 ex06.js

--- exercises/ex06/ex06.js
+++ exercises/ex06/ex06.js
@@ -6,4 +6,10 @@ function gerarSequencia() {
     for (let i = 1; i <= numTermo; i++) {
         fibonacci[i+1] = fibonacci[i] + fibonacci[i-1];
     }
+    res.innerHTML = `<h2>A sequência com ${numTermo} termos é</h2>`;
+    res.innerHTML += `<p>`;
+    for (let i = 0; i < fibonacci.length; i++) {
+        res.innerHTML += `<strong>... ${fibonacci[i]}</strong> `;
+    }
+    res.innerHTML += `</p>`;
 }

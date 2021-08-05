d5e5386d78892a3c16f48dbc710b49ec1a2d4015
Leandro Gabriel <enviaprolegabriel@gmail.com>
2021-08-05 09:14:42 -0300

    Update ex07.js

--- exercises/ex07/ex07.js
+++ exercises/ex07/ex07.js
@@ -48,3 +48,12 @@ function calcularDivCinco(array) {
     }
     return numDivCinco;
 }
+function calcularSomaPares(array) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        if ((array[i] % 2) == 0) {
+            total += array[i];
+        }
+    }
+    return total;
+}
